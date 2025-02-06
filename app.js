9fc3c8f5c428cc17e3be0375f04a8ce8baa138ad
José Augusto <josecouto.dev@gmail.com>
2025-02-06 15:32:56 -0300

    Commit inicial - função de adicionar amigo concluida

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+
+    let nomedoAmigo = document.querySelector('input').value.trim;
+    listaDeAmigos.push(nomedoAmigo);
+    console.log(listaDeAmigos);
+}
+
+adicionarAmigo();
+
+function atualizarListaDeAmigos() {
+
+}
+
+function sortearAmigo() {
+
+}
