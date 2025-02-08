579fec3b467335f6cb949829b56d52f353d6640a
José Augusto <josecouto.dev@gmail.com>
2025-02-08 11:54:52 -0300

    Concluçao das funções de atualização da lista e de sorteo dos amigos.

--- app.js
+++ app.js
@@ -12,20 +12,39 @@ function adicionarAmigo() {
         listaDeAmigos.push(nomeDoAmigo);
         console.log(listaDeAmigos);
         limparCampo();
+        atualizarListaDeAmigos()
     } 
 }
 
 adicionarAmigo();
 
 function atualizarListaDeAmigos() {
+    let listaAtualizada = document.getElementById('listaAmigos');
+    listaAtualizada.innerHTML = '';
 
-}
+    listaDeAmigos.forEach(listaDeAmigos => {
+        let itenList = document.createElement('li');
+        itenList.textContent = listaDeAmigos;
+        listaAtualizada.appendChild(itenList);
+    });
+}  
 
 function sortearAmigo() {
-
+    if (listaDeAmigos.length === 0) {
+       alert('É necessário adicionar amigos a lista antes do sorteio!');
+    } else {
+        document.getElementById('Sortear').removeAttribute('disabled');
+        let indice = parseInt(Math.random() * listaDeAmigos.length);
+        Math.floor(indice);
+        let amigoSorteado = listaDeAmigos[indice];
+        document.getElementById('resultado').innerHTML = `O amigo secreto é ${amigoSorteado}`;
+        listaDeAmigos = [];
+    }    
 }
+
+sortearAmigo(); 
 
 function limparCampo() {
     nomeDoAmigo = document.querySelector('input');
-    nomeDoAmigo.value = ''
+    nomeDoAmigo.value = '';
 }
